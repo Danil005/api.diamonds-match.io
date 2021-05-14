1cbca7b00f8ab1526863c8185726c7194107c3f7
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-05-14 17:39:56 +0300

    Update UtilsController.php

--- app/Http/Controllers/Api/v1/UtilsController.php
+++ app/Http/Controllers/Api/v1/UtilsController.php
@@ -21,7 +21,7 @@ class UtilsController extends Controller
 
         $cities = Cities::where(function(Builder $query) use ($data){
 
-            $query->where('LOWER(title_ru)', 'LIKE', '%'.$data['title'].'%');
+            $query->where('title_ru', 'ILIKE', '%'.$data['title'].'%');
 
         })->limit(25)->get(['title_ru', 'title_en']);
 
