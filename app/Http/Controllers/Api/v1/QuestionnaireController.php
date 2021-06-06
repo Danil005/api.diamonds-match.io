6fa52da295b47f36fcd66de9c491177eb1811459
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-06 12:40:52 +0300

    Update QuestionnaireController.php

--- app/Http/Controllers/Api/v1/QuestionnaireController.php
+++ app/Http/Controllers/Api/v1/QuestionnaireController.php
@@ -582,7 +582,7 @@ class QuestionnaireController extends QuestionnaireUtils
             $country_was = explode(',',$result['my_information']['countries_was']);
             $place = new Countries();
             foreach ($country_was as $item) {
-                $place->orWhere('title_en', 'ILIKE', $place);
+                $place->orWhere('title_en', 'ILIKE', $item);
             }
             $place = $place->get(['title_ru']);
             if( $place != null )
