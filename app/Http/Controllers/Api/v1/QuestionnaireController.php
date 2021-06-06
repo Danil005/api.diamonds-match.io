ea467bb0ec1a46eea0349cec30a0003f18d8afaf
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-06 21:08:24 +0300

    Update QuestionnaireController.php

--- app/Http/Controllers/Api/v1/QuestionnaireController.php
+++ app/Http/Controllers/Api/v1/QuestionnaireController.php
@@ -547,7 +547,7 @@ class QuestionnaireController extends QuestionnaireUtils
         if (!$request->has('path'))
             $this->response()->error()->setMessage('Вы должны указать path фотографии')->send();
 
-        Storage::delete($request->path);
+        Storage::disk('public')->delete(str_replace('storage/', 'public/', $request->path));
 
         $this->response()->success()->setMessage('Фотография была удалена')->send();
     }
