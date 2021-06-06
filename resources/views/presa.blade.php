8dc84a70417dd5f990618b15aed15e8144952f77
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-07 00:51:03 +0300

    1

--- resources/views/presa.blade.php
+++ resources/views/presa.blade.php
@@ -18,9 +18,8 @@
 {{--            Позвольте нам создать их!--}}
 {{--        </div>--}}
 {{--    </div>--}}
-    <div class="page" id="hello">
-        <img src="{{ asset('assets/img/BackgroundPage.jpg') }}" class="transparent-background" alt="transparent"/>
-        <img src="{{ asset('assets/img/White.png') }}" class="transparent-background"  style="opacity: 0.7;" alt="transparent"/>
+    <div class="page background" id="hello">
+        <img src="{{ asset('assets/img/White.png') }}" class="transparent-background" alt="transparent"/>
 
         <div>
             <img src="{{ asset('assets/img/Logo.png') }}" class="logo-right" alt="Logo"/>
