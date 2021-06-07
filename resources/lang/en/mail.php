0253b80cf76a5af1d38a08bc32220140a968f0f6
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-07 04:04:15 +0300

    Add en mail

---
+++ resources/lang/en/mail.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+    'hello' => 'Dear, :name',
+    'receive_mail' => 'We would like to offer you the following tariff plans::',
+    'tariffs' => 'Tariffs',
+    'month' => 'Month',
+    'months' => 'Month',
+    'go_to_pay' => 'Proceed to checkout',
+    'connecting_instagram' => 'Follow us on Instagram',
+    'phone' => 'Phone',
+    'email' => 'Email',
+    'address' => 'Address',
+    'vip' => [
+        't1' => [
+            'v1' => 'Not less than 50  profiles and not more than 100 profiles',
+            'v2' => '3 Dates',
+            'v3' => 'Consultation before the first date'
+        ],
+        't2' => [
+            'v1' => 'Not less than 200  profiles and not more than 300 profiles',
+            'v2' => '9 Dates',
+            'v3' => 'Consultation before the first date',
+            'v4' => 'One session with Psychologist'
+        ],
+        't3' => [
+            'v1' => 'More than 500 Profiles',
+            'v2' => '20 Dates',
+            'v3' => 'Consultation before every date',
+            'v4' => 'One session with Psychologist',
+            'v5' => 'Astrological compatibility analysis with 3 candidates '
+        ]
+    ],
+    'platinum' => [
+        't1' => [
+            'v1' => 'At least 100 profiles',
+            'v2' => '5 Dates',
+            'v3' => 'Consultation before the first date',
+            'v4' => 'One session with Psychologist',
+            'v5' => 'Client’s natal chart',
+            'v6' => 'General astrologer’s consultation on compatibility'
+        ],
+        't2' => [
+            'v1' => 'At least 500 profiles',
+            'v2' => '15 Dates',
+            'v3' => 'Consultation before the first date',
+            'v4' => 'Professional photoset',
+            'v5' => 'Phycologist consultation ',
+            'v6' => 'Client’s natal chart',
+            'v7' => 'General astrologer’s consultation on compatibility with selected girls'
+        ],
+        't3' => [
+            'v1' => 'At least 500 profiles',
+            'v2' => '30 Dates',
+            'v3' => 'Consultation before every date',
+            'v4' => 'Professional photoset',
+            'v5' => 'Phycologist consultation ',
+            'v6' => 'Client’s natal chart',
+            'v7' => 'General astrologer’s consultation on compatibility with selected girls',
+            'v8' => 'Image maker services'
+        ]
+    ]
+];
