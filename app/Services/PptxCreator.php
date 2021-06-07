d35cf8c0ab6fb91ad02d0502b10d29862bf6c261
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-07 03:18:06 +0300

    Update PptxCreator.php

--- app/Services/PptxCreator.php
+++ app/Services/PptxCreator.php
@@ -42,6 +42,7 @@ class PptxCreator
         $slides = '';
         for($i = 1; $i <= 5; $i++) $slides .= '/var/www/html/storage/app/public/pptx/generate/'.$id.'/s'.$i . ' ';
         $slides = trim($slides);
+        echo $slides;
 
         $stream = ssh2_exec($connection, 'convert ' . $slides . ' /var/www/html/storage/app/public/pptx/generate/'.$id.'/presentation.pdf');
         stream_set_blocking($stream, true);
