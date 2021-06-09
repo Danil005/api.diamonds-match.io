86d845111717f2643ae6853b043c04b779aaad0b
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-09 20:33:24 +0300

    Create pptx.php

---
+++ resources/lang/en/pptx.php
@@ -0,0 +1,61 @@
+<?php
+
+
+return [
+    'title1' => 'IMAGINE A PERFECT RELATIONSHIP',
+    'title2' => 'Let us create them!',
+    'empty' => 'Not indicated',
+
+    'slide2' => [
+        'p1' => 'Country:',
+        'p2' => 'Ethics:',
+        'p3' => 'Place of living:',
+        'p4' => 'City of birth:',
+        'p5' => 'Are you considering coming:',
+    ],
+    'slide4' => [
+        't1' => [
+            't' => 'COMMON DATA',
+            'p1' => 'Zodiac sign:',
+            'p2' => 'Height:',
+            'p3' => 'Weight:',
+            'p4' => 'Body type:',
+            'p5' => 'Hair color:',
+            'p6' => 'Status:',
+            'p7' => 'Children:',
+            'p8' => 'Do you want children:',
+            'p9' => 'Smoking:',
+            'p10' => 'Alcohol:',
+            'p11' => 'Religion:',
+            'p12' => 'Languages:',
+        ],
+        't2' => [
+            't' => 'EDUCATION AND HEALTH',
+            'p1' => 'Education:',
+            'p2' => 'Work:',
+            'p3' => 'The salary:',
+            'p4' => 'Health problems:',
+            'p5' => 'Allergy:',
+        ]
+    ],
+    'slide5' => [
+        't1' => [
+            't' => 'INTERESTS',
+            'p1' => 'Do you love pets:',
+            'p2' => 'Are there pets and which ones:',
+            'p3' => 'Books or films:',
+            'p4' => 'Relaxation:',
+            'p5' => 'Countries visited:',
+            'p6' => 'Countries he dreams of visiting:',
+            'p7' => 'The best gift for you:',
+            'p8' => 'Hobby:',
+            'p9' => 'Life credo:',
+        ],
+        't2' => [
+            't' => 'ANSWERS ON QUESTIONS',
+            'p1' => 'What traits turn you off in people?',
+            'p2' => 'How do you feel about the significant age difference between partners?',
+            'p3' => 'What do you think are your talents?'
+        ]
+    ]
+];
