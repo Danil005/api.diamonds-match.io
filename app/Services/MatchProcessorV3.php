6dc5a6f24eae9765c330cd2c2356a9c74afffe66
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-09 20:02:56 +0300

    Update MatchProcessorV3.php

--- app/Services/MatchProcessorV3.php
+++ app/Services/MatchProcessorV3.php
@@ -422,7 +422,7 @@ class MatchProcessorV3
                 $pqResult = round(($r1 + $r2) / 2);
 
                 if( $q1->id == 25 && $q2->id == 37 ) {
-                    dd($pqResult, $r1, $r2, '1: ', $pqWant1, $pqMy1, '2: ', $pqWant2, $pqMy2, count($fields));
+                    dd($pqResult, $r1, $r2, '1: ', $pqWant1, $pqMy1, $this->pqMatch($pqMy1, $pqWant1),  '2: ', $pqWant2, $pqMy2, $this->pqMatch($pqMy2, $pqWant2), count($fields));
                 }
 
                 # Сравнение тестов
