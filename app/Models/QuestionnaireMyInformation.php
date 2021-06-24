c8e3690502c947227ff8a2e5c549413f8c0820d5
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-24 18:21:10 +0300

    1

--- app/Models/QuestionnaireMyInformation.php
+++ app/Models/QuestionnaireMyInformation.php
@@ -19,7 +19,6 @@ class QuestionnaireMyInformation extends Model
         'best_gift', 'hobbies', 'kredo', 'features_repel', 'age_difference',
         'films', 'songs', 'ideal_weekend', 'sleep', 'doing_10', 'signature_dish',
         'clubs', 'best_gift_received', 'talents', 'name', 'birthday',
-        'work_name', 'education_name', 'skype', 'about_us',
-        'about_us_ads', 'about_us_friends', 'about_us_manager', 'about_us_forum'
+        'work_name', 'education_name', 'skype'
     ];
 }
