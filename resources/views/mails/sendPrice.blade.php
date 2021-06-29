c14e9b1f83f00f9766a08ff45adb41025a8eefcc
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-06-29 16:59:10 +0300

    1

--- resources/views/mails/sendPrice.blade.php
+++ resources/views/mails/sendPrice.blade.php
@@ -142,7 +142,7 @@
                                                     <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                                                         <tr>
                                                             <td align="left" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:18px;color:#A9A9A9;font-size:12px">
-                                                                {{ __('mail.tariffs', locale: $lang) }} VIP</p></td>
+                                                                {{ __('mail.tariffs', locale: $lang) }} Gold</p></td>
                                                         </tr>
                                                     </table></td>
                                             </tr>
