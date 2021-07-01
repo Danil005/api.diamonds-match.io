d6206dddbc926d3a674cc70f30fc7d1434d6e64e
Danil Sidorenko <danilsidorenko00@gmail.com>
2021-07-01 08:47:49 +0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/yookassa/callback'
+        '/yookassa/callback',
+        '/stripe/webhook'
     ];
 }
